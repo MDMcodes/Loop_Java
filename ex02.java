1dc6d887e26ccfbaf146ea1be7af6d919201fa59
MDMcodes <matheusdonadelmarques08@gmail.com>
2025-03-19 13:15:57 -0300

    a

--- ex02.java
+++ ex02.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 public class ex02 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.printLn("Estado Civil: C casado, S solteiro, V viuvo, D divorciado");
+        System.out.println("Estado Civil: C casado, S solteiro, V viuvo, D divorciado");
         System.out.println("Escolha seu Estado Civil: ");
         String estadoCivil = scanner.nextLine();
 
-        if (estadoCivil.equalsIgnoreCase(C)){
+        if (estadoCivil.equalsIgnoreCase("C")){
             System.out.println("Você é casado");
         }
-        else if (estadoCivil.equalsIgnoreCase(S)){
+        else if (estadoCivil.equalsIgnoreCase("S")){
             System.out.println("Você é solteiro");
         }
-        else if (estadoCivil.equalsIgnoreCase(V)){
+        else if (estadoCivil.equalsIgnoreCase("V")){
             System.out.println("Você é viuvo");
         }
-        else if (estadoCivil.equalsIgnoreCase(D)){
+        else if (estadoCivil.equalsIgnoreCase("D")){
             System.out.println("Você é divorciado");
         }
         else {
             System.out.println("Estado Civil inválido");
         }
         scanner.close();
-        }
-}
+    }
+};
+
